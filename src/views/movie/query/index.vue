f394f9c8fbfeef7e881b0e73820057a2502d90d9
shibaoping <shibaoping@test.com>
2019-08-21 14:25:07 +0800

    fix bug

--- src/views/movie/query/index.vue
+++ src/views/movie/query/index.vue
@@ -55,49 +55,51 @@
           </div>
         </el-collapse-item>
       </el-collapse>
-      <el-table :data="tableData" style="width: 100%" size="mini" max-height="100%">
-        <el-table-column label="日期" width="180">
-          <template slot-scope="scope">
-            <i class="el-icon-time" />
-            <span style="margin-left: 10px">{{ scope.row.date }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column label="姓名" width="180">
-          <template slot-scope="scope">
-            <el-popover trigger="click" placement="top">
-              <p>姓名: {{ scope.row.name }}</p>
-              <p>住址: {{ scope.row.address }}</p>
-              <div slot="reference" class="name-wrapper">
-                <el-button size="mini">{{ scope.row.name }}</el-button>
-              </div>
-            </el-popover>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="姓名"
-          width="180"
-        >
-          <template slot-scope="scope">
-            <el-popover trigger="hover" placement="top">
-              <p>姓名: {{ scope.row.name }}</p>
-              <p>住址: {{ scope.row.address }}</p>
-              <div slot="reference" class="name-wrapper">
-                <el-tag size="medium">{{ scope.row.name }}</el-tag>
-              </div>
-            </el-popover>
-          </template>
-        </el-table-column>
-        <el-table-column label="操作">
-          <template slot-scope="scope">
-            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <div class="pageCounter">
-        <el-pagination  background layout="prev, pager, next" :total="1000"></el-pagination>
+      <div class="">
+        <el-table :data="tableData" style="width: 100%" size="mini" max-height="340px">
+            <el-table-column label="日期" width="180">
+              <template slot-scope="scope">
+                <i class="el-icon-time" />
+                <span style="margin-left: 10px">{{ scope.row.date }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="姓名" width="180">
+              <template slot-scope="scope">
+                <el-popover trigger="click" placement="top">
+                  <p>姓名: {{ scope.row.name }}</p>
+                  <p>住址: {{ scope.row.address }}</p>
+                  <div slot="reference" class="name-wrapper">
+                    <el-button size="mini">{{ scope.row.name }}</el-button>
+                  </div>
+                </el-popover>
+              </template>
+            </el-table-column>
+            <el-table-column
+              label="姓名"
+              width="180"
+            >
+              <template slot-scope="scope">
+                <el-popover trigger="hover" placement="top">
+                  <p>姓名: {{ scope.row.name }}</p>
+                  <p>住址: {{ scope.row.address }}</p>
+                  <div slot="reference" class="name-wrapper">
+                    <el-tag size="medium">{{ scope.row.name }}</el-tag>
+                  </div>
+                </el-popover>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
       </div>
     </el-card>
+    <div class="pageCounter">
+      <el-pagination  background layout="prev, pager, next" :total="1000"></el-pagination>
+    </div>
   </div>
 </template>
 
